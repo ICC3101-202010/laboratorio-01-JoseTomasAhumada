d8daca34d013b67f1b9e98840cd3b6fd77dc79ef
José Tomás Ahumada <jtahumada@miuandes.cl>
2020-03-24 12:31:54 -0300

    Laboratio01 finished

---
+++ Laboratorio01/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Laboratorio01
+{
+    public class Persona
+    {
+        private string firstName;
+        private string lastName;
+
+        public Persona(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+        public string GetLastName()
+        { 
+            return lastName;
+        }
+        public int Lanzar()
+        {
+            Random aleatorio = new Random();
+            int resultado = aleatorio.Next(0, 3);
+            return resultado;
+
+        }
+    }
+}
---
+++ Laboratorio01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laboratorio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Bob", "Kunga");
+            Console.WriteLine("La persona " + p1.GetFirstName() + " " + p1.GetLastName() + " obtuvo " + p1.Lanzar());
+            Console.ReadKey();
+        }
+    }
+}
